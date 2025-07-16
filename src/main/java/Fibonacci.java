fe69068455568c59009c87b88880d2e1801f7b98
Anthony Knight <aknight0211@gmail.com>
2025-07-16 05:58:08 +0000

    I decided to use a for loop the would iterate through up to n the length of the sequence and had it ad the numbers in the way the sequence asks for

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,15 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int a = 0;
+        int b = 1;
+        for(int i = 2; i <= n; i++){
+            int next = a+b;
+            a = b;
+            b = next;
+        }
+        return b;
     }
 }
